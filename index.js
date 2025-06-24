9d5d77d4364b88e5d03b33ea1af9e996bb2be37d
jhonatanT04 <venotacu@gmail.com>
2025-06-23 20:55:41 -0500

    first commit

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.post('/posts', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor backend corriendo en http://0.0.0.0:${PORT}`);
 });
+
